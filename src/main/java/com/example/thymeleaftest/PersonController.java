6a2e672ee974fe50c947e75b367fd55f2a5c06f3
Teller501 <ande777j@stud.kea.dk>
2023-03-13 18:44:59 +0100

    added a Person class, and made a table that gets each person that is added with their name and age

--- src/main/java/com/example/thymeleaftest/PersonController.java
+++ src/main/java/com/example/thymeleaftest/PersonController.java
@@ -4,12 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+
 @Controller
 public class PersonController {
 
     @GetMapping("/")
     String getPeople(Model model){
         model.addAttribute("something", "this is coming from the controller");
+        model.addAttribute("people", Arrays.asList(
+                new Person("Anders Teller",20),
+                new Person("Nicolai Andersson", 38),
+                new Person("Victor Hanert",14)));
         return "people";
     }
 }
