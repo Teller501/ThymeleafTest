d8556c457ebc3e7e24954e7babc037d4d4635c33
Teller501 <ande777j@stud.kea.dk>
2023-03-13 18:34:26 +0100

    Initial commit - added Controller and basic html page

---
+++ src/main/java/com/example/thymeleaftest/PersonController.java
@@ -0,0 +1,15 @@
+package com.example.thymeleaftest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PersonController {
+
+    @GetMapping("/")
+    String getPeople(Model model){
+        model.addAttribute("something", "this is coming from the controller");
+        return "people";
+    }
+}
